d76fc44cde1a609beaed44d91f2b27a102141830
Jay Modi <mjrulesamrat@gmail.com>
2019-08-03 17:14:18 +0530

    Index page and Initial templates tree

--- backend/nuwill/views.py
+++ backend/nuwill/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class IndexView(View):
+
+    def get(self, request):
+        template_name = "index.html"
+        return render(request, template_name)
+
