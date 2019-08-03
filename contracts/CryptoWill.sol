5d752c153185f0336a4de5c1b8e4825d67575625
Jay Modi <mjrulesamrat@gmail.com>
2019-08-03 07:52:24 +0530

    Fix CryptoWill contract

--- contracts/CryptoWill.sol
+++ contracts/CryptoWill.sol
@@ -41,53 +41,55 @@ contract CryptoWill {
         require(destination.call.value(amount)(data));
     }
 
-    function transferOwnership(address newOwner) onlyOwner { // called by owner to change the ownership
+    function transferOwnership(address newOwner) public onlyOwner { // called by owner to change the ownership
         owner = newOwner;
         beneficiary = owner;
     }
+
     //called by owner to add/modify an heir; inheritance shares are directly propotional to the points assigned
-    function setHeir(address heir, uint8 inheritancePoints, uint periodInDays) onlyOwner returns (address) {
-		if (wallets[heir] == 0 && inheritancePoints > 0) {
+    function setHeir(address heir, uint8 inheritancePoints, uint periodInDays) public returns (address) {
+        if (wallets[heir] == 0 && inheritancePoints > 0) {
 			//wallets[heir] = factory.create(heir, periodInDays);
 		} else if (wallets[heir] > 0 && inheritancePoints == 0) {
 			CryptoWill(wallets[heir]).destroy();
 			delete wallets[heir];
 		}
-		totalPoints -= points[wallets[heir]];
-		points[wallets[heir]] = inheritancePoints;
-		totalPoints += inheritancePoints;
-		return wallets[heir];
-	}
-	/* called by anyone to give the benificiary full ownership of this account when his predecessor is inactive */
-	function unlock() {
-		require(beneficiary != owner); // already unlocked
-		HeritableWallet(owner).claimInheritance();
-		owner = beneficiary;
-		lastCheckInTime = now;
-	}
+        totalPoints -= points[wallets[heir]];
+        points[wallets[heir]] = inheritancePoints;
+        totalPoints += inheritancePoints;
+        return wallets[heir];
+    }
 
-	/* called by an heir to collect his share in the inheritance */
-	function claimInheritance() onlyHeir {
-		require (beneficiary == owner); // account is locked
-		if (now <= lastCheckInTime + checkInPeriod) throw; // owner was active recently
-		uint8 heirPoints = points[msg.sender];
-		uint amount = this.balance * heirPoints / totalPoints; // compute amount for current heir
-		totalPoints -= heirPoints;
-		delete points[msg.sender];
-		require(!msg.sender.send(amount)); // transfer proper amount to heir or revert state if it fails
-		totalPoints += heirPoints;
-		points[msg.sender] = heirPoints;
-		if (totalPoints == 0) { // last heir, destroy empty contract
-			selfdestruct(owner);
-		}
-	}
-	/* called by owner to terminate this contract */
-	function destroy() onlyOwner {
-		selfdestruct(owner);
-	}
-	
-	function getOwner() returns (address) { return owner; }
-	function getBeneficiary() returns (address) { return beneficiary; }
+    /* called by anyone to give the benificiary full ownership of this account when his predecessor is inactive */
+    function unlock() public {
+        require(beneficiary != owner); // already unlocked
+        HeritableWallet(owner).claimInheritance();
+        owner = beneficiary;
+        lastCheckInTime = now;
+    }
+
+    /* called by an heir to collect his share in the inheritance */
+    function claimInheritance() public OnlyHeir {
+        require (beneficiary == owner, "Account is locked"); // account is locked
+        require (now > lastCheckInTime + checkInPeriod, "owner was active recently");  // owner was active recently
+        uint8 heirPoints = points[msg.sender];
+        uint amount = this.balance * heirPoints / totalPoints; // compute amount for current heir
+        totalPoints -= heirPoints;
+        delete points[msg.sender];
+        require(!msg.sender.send(amount)); // transfer proper amount to heir or revert state if it fails
+        totalPoints += heirPoints;
+        points[msg.sender] = heirPoints;
+        if (totalPoints == 0) { // last heir, destroy empty contract
+            selfdestruct(owner);
+        }
+    }
+    /* called by owner to terminate this contract */
+    function destroy() internal onlyOwner {
+        selfdestruct(owner);
+    }
+
+    function getOwner() public returns (address) { return owner; }
+    function getBeneficiary() public returns (address) { return beneficiary; }
 	
 }
 
